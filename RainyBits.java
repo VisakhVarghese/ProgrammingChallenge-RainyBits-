455b933147bfeaecec19ed3d92faa5ecea32cbb7
VisakhVarghese <visakhvarghese63@gmail.com>
2021-07-12 18:13:46 +0530

    first commit

--- RainyBits.java
+++ RainyBits.java
@@ -16,7 +16,7 @@ class ReadFile {
         System.out.println("-------------Read From Text File---------------"+"\n");
 
         //File Reads here
-        File file = new File("/home/visakh/Downloads/rainbitsSample.txt");
+        File file = new File("/home/visakh/Downloads/resumesample.txt");
         FileReader reader = new FileReader(file);
         BufferedReader bfr = new BufferedReader(reader);
         String str;
@@ -39,36 +39,67 @@ class ReadFile {
 
 class ExtractFile extends ReadFile {
 
-    public void extractFile(){     
+    public void extractFile(){  
 
-    try{  
-        
         System.out.println("");
         System.out.println("-----------Extract the Values----------------"+"\n");
 
-        // Values Like Name, Email, Address, Phone
-        String[] searchArray = { "Name","Phone", "Email", "Address"};
+           // if you wanna use array to extract value
+           String[] searchArray = { "Name","Phone","Mobile" ,"Email","E-mail", "Address"}; 
+
+        //if you wanna extact value by keyboard
         File file = new File("/home/visakh/Downloads/rainbitsSample.txt");
-        FileReader reader = new FileReader(file);
+        FileReader reader = null;
+        Scanner sc= new Scanner(System.in);
+        String searchWord, str;
+        int n;
+        System.out.println("Please select mode:");
+        System.out.println("1. By Keyboard");
+        System.out.println("2. By Array");
+        n = sc.nextInt();
+       
+    
+
+    try{  
+        
+        reader = new FileReader(file);
         BufferedReader bfr = new BufferedReader(reader);
-        String str;       
 
-        while((str=bfr.readLine())!=null){
-            for(int i=0; i<=searchArray.length - 1; i++){
+                if(n==1){
+                    Scanner s = new Scanner(System.in);
+                    System.out.println("Enter the word you want to search: ");
+                    searchWord = s.nextLine();
+                    while((str=bfr.readLine())!=null){
+                        if(str.contains(searchWord)){
+                            System.out.println(str);
+                        }
+                    }
+                    s.close();
+                }else if(n==2){ 
 
-                if(str.contains(searchArray[i])){
-                    System.out.println(str);
+                       while((str=bfr.readLine())!=null){
+                        for(int i=0; i<=searchArray.length - 1; i++){
+            
+                            if(str.contains(searchArray[i])){
+                                System.out.println(str);
+                            }
+                        }
+                    } 
+                }else{
+                    System.out.println("Invalid Option!");
                 }
-           }
-        }  
-        bfr.close(); 
+
+                bfr.close();
+  
+    
 
     }catch(FileNotFoundException e1){
         System.out.println(e1.getMessage());
     }catch(IOException e2){
         System.out.println(e2.getMessage());
+    }finally{
+        sc.close();
     }
-
     }
 
 }
